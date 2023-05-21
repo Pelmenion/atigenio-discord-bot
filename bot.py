a0dbae23f8be3ed4bd5cc25a1ce3dcbcbcd97684
Pelmenion <mita54492@gmail.com>
2023-05-21 23:24:35 +0400

    0.0.3 Небольшая коррекция кода, проблемы с аргументами в команде

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
-
 import discord
+from discord.ext import commands
 
-
-
-TOKEN = 'токен'
+TOKEN = 'типо токен'
 PREFIX = '/'
 
 admin_roles = [1107669935087431801, 1107669885317824513, 1107298355614339122]
@@ -45,6 +43,22 @@ async def help(inter):
     )
     await inter.response.send_message(embed=embed_home)
 
-
+@bot.slash_command(name='kick',
+                   description='Кикает с сервера указанного пользователя',
+                   )
+@commands.has_permissions(administrator = True)
+async def kick(
+    inter,
+    member: Option(discord.member, description='Пользователь, которого наказывают.', required=True),
+    reason: Option(str, description='Причина кика пользователя', required=False)
+               ):
+    embed = discord.Embed(title=f'Пользователь `{member}` был кикнут',
+                          color=discord.Colour.green()
+                          )
+    if reason != None:
+        embed.set_footer(text='По причине {reason}')
+    
+    await inter.response.send_message(embed=embed)
+    
 
 bot.run(TOKEN)
