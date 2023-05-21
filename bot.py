972a71a8fff34d70400ab0e298b7fdc6d958333e
Pelmenion <mita54492@gmail.com>
2023-05-21 22:01:22 +0400

    0.0.2 Переделка кода под py-cord(разновид Discord)

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 
-import disnake
-from disnake.ext import commands
+import discord
 
-TOKEN = 'ф'
+
+
+TOKEN = 'токен'
 PREFIX = '/'
 
 admin_roles = [1107669935087431801, 1107669885317824513, 1107298355614339122]
@@ -10,16 +11,16 @@ admin_roles = [1107669935087431801, 1107669885317824513, 1107298355614339122]
 
 
 
-intents = disnake.Intents.all()
+intents = discord.Intents.all()
 intents.members = True
-bot = commands.Bot(command_prefix = PREFIX, intents = intents)
+bot = discord.Bot(command_prefix = PREFIX, intents = intents)
 
-bot.remove_command('help')
+
 
 @bot.event
 async def on_ready():
     print('Бот подключен!')
-    await bot.change_presence(activity=disnake.Game(name=f'{PREFIX}help'))
+    await bot.change_presence(activity=discord.Game(name=f'{PREFIX}help'))
 
 
 @bot.slash_command(name='ping',
@@ -27,20 +28,20 @@ async def on_ready():
 async def ping(inter):
     ping = bot.latency
     ping = round(ping, 2)
-    embed = disnake.Embed(
+    embed = discord.Embed(
         title='Понг!:ping_pong:',
         description=f'Пинг: `{ping*1000} mc`',
-        color=disnake.Colour.green()
+        color=discord.Colour.green()
     )
     await inter.response.send_message(embed=embed)
 
 @bot.slash_command(name='help',
                    description='Выводит список команд')
 async def help(inter):
-    embed_home = disnake.Embed(
+    embed_home = discord.Embed(
         title='Доступные команды',
         description='Все доступные команды видны в `/`, и недоступные тоже xD',
-        color=disnake.Colour.green()
+        color=discord.Colour.green()
     )
     await inter.response.send_message(embed=embed_home)
 
